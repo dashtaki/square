b227a36fdc2c3595df09d941750b9306b0c383c6
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2020-12-14 22:09:13 +0200

    Create square component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+
+const DECLARATIONS = [AppComponent, SquareComponent];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [...DECLARATIONS],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
