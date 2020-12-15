7ba8a64d2b27b54e1b343aa3c6e90f5654ffd2b5
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2020-12-15 23:10:07 +0200

    Write tests for TileService

--- src/app/tile/service/tile.service.spec.ts
+++ src/app/tile/service/tile.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TileService } from './tile.service';
 
 describe('TileService', () => {
@@ -12,5 +11,17 @@ describe('TileService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return FALSE when id is not breakable', () => {
+    const isBreakable = service.isBreakable(7);
+
+    expect(isBreakable).toBeFalse();
+  });
+
+  it('should return TRUE when id is breakable', () => {
+    const isBreakable = service.isBreakable(11);
+
+    expect(isBreakable).toBeTrue();
   });
 });
