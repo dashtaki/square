374826e22ea93a729885acbfca602a3cee322ed7
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2020-12-15 22:42:29 +0200

    Write tests for SpinnerComponent

--- src/app/spinner/spinner.component.spec.ts
+++ src/app/spinner/spinner.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SpinnerComponent } from './spinner.component';
+import { By } from '@angular/platform-browser';
+import { SpinnerService } from './service/spinner.service';
 
 describe('SpinnerComponent', () => {
   let component: SpinnerComponent;
   let fixture: ComponentFixture<SpinnerComponent>;
+  let mockSpinnerService;
 
   beforeEach(async () => {
+    mockSpinnerService = jasmine.createSpyObj(['loading']);
+    (mockSpinnerService as any).loading = false;
+
     await TestBed.configureTestingModule({
       declarations: [SpinnerComponent],
+      providers: [{ provide: SpinnerService, useValue: mockSpinnerService }],
     }).compileComponents();
   });
 
@@ -20,5 +27,29 @@ describe('SpinnerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not show spinner in first initialization', () => {
+    const spinner = fixture.debugElement.query(By.css('.spinner__wrapper'));
+
+    expect(spinner).toBeNull();
+  });
+
+  it('should show spinner', () => {
+    (mockSpinnerService as any).loading = true;
+    fixture.detectChanges();
+    const spinner = fixture.debugElement.query(By.css('.spinner__wrapper'));
+
+    expect(spinner.nativeElement).toBeTruthy();
+  });
+
+  it('should show 4 cubes as child', () => {
+    (mockSpinnerService as any).loading = true;
+    fixture.detectChanges();
+    const spinnerCubes = fixture.debugElement.queryAll(
+      By.css('.spinner__cube')
+    );
+
+    expect(spinnerCubes.length).toBe(4);
   });
 });
