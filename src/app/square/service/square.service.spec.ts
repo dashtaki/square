04443dfa84fa048367667e295dfd566c426eef44
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2020-12-15 22:56:01 +0200

    Write tests for SquareService

--- src/app/square/service/square.service.spec.ts
+++ src/app/square/service/square.service.spec.ts
@@ -1,19 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SquareService } from './square.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('SquareService', () => {
   let service: SquareService;
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(SquareService);
+    httpMock = TestBed.get(HttpTestingController);
+    httpClient = TestBed.get(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getPosts', () => {
+    service.getPosts().subscribe();
+
+    httpMock.expectOne('https://jsonplaceholder.typicode.com/posts');
+    httpMock.verify();
   });
 });
