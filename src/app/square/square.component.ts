4c2d838a6612aa058bd7dc060223fc677c31ea17
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2020-12-16 22:03:36 +0200

    Use SubSink to unsubscribe on component destroy

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { SquareService } from './service/square.service';
 import { IPost } from '../interface/IPost';
 import { SpinnerService } from '../spinner/service/spinner.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.scss'],
 })
-export class SquareComponent {
+export class SquareComponent implements OnDestroy {
   public posts: IPost[] = [];
+  private _subs = new SubSink();
 
   constructor(
     private squareService: SquareService,
@@ -20,13 +22,19 @@ export class SquareComponent {
 
   private fetchAllPosts(): void {
     this.spinnerService.showSpinner();
-    this.squareService.getPosts().subscribe((posts: IPost[]) => {
-      this.posts = posts;
-      this.spinnerService.hideSpinner();
-    });
+    this._subs.sink = this.squareService
+      .getPosts()
+      .subscribe((posts: IPost[]) => {
+        this.posts = posts;
+        this.spinnerService.hideSpinner();
+      });
   }
 
   public trackByFn(index: number): number {
     return index;
+  }
+
+  ngOnDestroy(): void {
+    this._subs.unsubscribe();
   }
 }
