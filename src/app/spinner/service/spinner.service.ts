667f02c9bf1633089534e07bf0190ec9dbd339a5
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2020-12-16 21:55:42 +0200

    Use Ngrx tos show/hide spinner while fetching data from API

--- src/app/spinner/service/spinner.service.ts
+++ src/app/spinner/service/spinner.service.ts
@@ -1,22 +1,15 @@
 import { Injectable } from '@angular/core';
+import * as fromRoot from '../../ngrx';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpinnerService {
-  private _loading = false;
+  constructor(private store: Store<fromRoot.State>) {}
 
-  constructor() {}
-
-  get loading(): boolean {
-    return this._loading;
-  }
-
-  public hideSpinner(): void {
-    this._loading = false;
-  }
-
-  public showSpinner(): void {
-    this._loading = true;
+  get loading(): Observable<boolean> {
+    return this.store.select(fromRoot.getLoading);
   }
 }
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IPost } from '../interface/IPost';
 import { Store } from '@ngrx/store';
-import { SpinnerService } from '../spinner/service/spinner.service';
 import * as fromRoot from '../ngrx/index';
 import * as squareActions from './store/action/square-action';
 
@@ -13,21 +12,16 @@ import * as squareActions from './store/action/square-action';
 export class SquareComponent {
   public posts: IPost[] = [];
 
-  constructor(
-    private spinnerService: SpinnerService,
-    private store: Store<fromRoot.State>
-  ) {
+  constructor(private store: Store<fromRoot.State>) {
     this.fetchAllPosts();
   }
 
   private fetchAllPosts(): void {
-    this.spinnerService.showSpinner();
     this.store.dispatch(new squareActions.PostsAction());
     this.store.select(fromRoot.getSquare).subscribe((posts: IPost[]) => {
       if (posts) {
         this.posts = posts;
       }
-      this.spinnerService.hideSpinner();
     });
   }
 
